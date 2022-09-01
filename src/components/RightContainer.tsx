86a6023938ed0471d86d0e15930cbbd857947942
Prateek Soni <prateek_soni@outlook.com>
2022-09-02 02:31:52 +0530

    add send message input
    Signed-off-by: Prateek Soni <prateek_soni@outlook.com>

--- src/components/RightContainer.tsx
+++ src/components/RightContainer.tsx
@@ -1,8 +1,10 @@
 import type { Contact } from "@/types/appTypes";
 import { User } from "@prisma/client";
 import { FC, useState } from "react";
+import { FiSend } from "react-icons/fi";
 import { styled } from "stitches.config";
 import Avatar from "./Avatar";
+import Button from "./Button";
 import ContactEmailForm from "./ContactEmailForm";
 import Heading from "./Heading";
 
@@ -44,6 +46,17 @@ const RightContainer: FC<RightContainerProps> = ({ contact }) => {
           contact.label
         )}
       </ContactContainer>
+      {contact && (
+        <ChatContainer>
+          <div className="messages"></div>
+          <ChatInputContainer>
+            <ChatInput placeholder="Send message..." />
+            <SendButton>
+              <FiSend />
+            </SendButton>
+          </ChatInputContainer>
+        </ChatContainer>
+      )}
     </Right>
   );
 };
@@ -77,4 +90,37 @@ const UserLabelContainer = styled("div", {
       fontSize: "0.9rem",
     },
   },
+});
+
+const ChatContainer = styled("div", {
+  padding: "1rem",
+  display: "flex",
+  flexDirection: "column",
+  height: "calc(100vh - 90px)",
+
+  ".messages": {
+    flexGrow: 1,
+  },
+});
+
+const ChatInputContainer = styled("div", {
+  position: "relative",
+});
+
+const ChatInput = styled("input", {
+  borderRadius: "5px",
+  backgroundColor: "$dark",
+  border: "none",
+  padding: "1rem",
+  width: "100%",
+  fontSize: "1rem",
+});
+
+const SendButton = styled(Button, {
+  fontSize: "1.4rem",
+  position: "absolute",
+  top: "50%",
+  right: "1rem",
+  transform: "translateY(-50%)",
+  borderRadius: "999px",
 });
